94b940809a47928dd8461dd394e4b1bd5070e768
duisada <duisadasui@gmail.com>
2023-05-23 20:38:21 +0200

    Ajout de ligne de commande

--- FichierTestGit.c
+++ FichierTestGit.c
@@ -7,6 +7,7 @@ int main()
 	printf("Enter a number : \n");
 	scanf("%d", &nb);
 	printf("Your number is : %d\n", nb);
-
+	printf("ajout d'une commande\n");
+	printf("ajout d'une seconde ligne\n");
 	return 0;
 }
